d6aaf183e7dc4b942e86a86105067e3f93b14155
ChaynikArbyz <romikbro.133@gmail.com>
2025-07-06 21:35:52 +0300

    added second task

---
+++ TaxiCounter/TaxiCounter/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiCounter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Console.WriteLine("--Калькулятор ціни таксі--\nЦіна за 1 км - 10 грн\nЦіна за 1 хвилину простою - 2 грн\nМінімальна ціна таксі - 50грн\n");
+            Console.WriteLine("Введіть відстань у кілометрах:");
+
+            bool distSuccess = double.TryParse(Console.ReadLine(), out double dist);
+            if (!distSuccess)
+            {
+                Console.WriteLine("Невірний формат відстанні");
+                return;
+            }
+            else if (dist < 0)
+            {
+                Console.WriteLine("Відстань не може бути від'ємною");
+                return;
+            }
+
+            Console.WriteLine("Введіть час очікування у хвилинах:");
+            bool timeSuccess = double.TryParse(Console.ReadLine(), out double time);
+            if (!timeSuccess)
+            {
+                Console.WriteLine("Невірний формат часу");
+                return;
+            }
+            else if (time < 0)
+            {
+                Console.WriteLine("Час не може бути від'ємним");
+                return;
+            }
+
+            double price = dist * 10 + time * 2;
+
+            if (price < 50)
+            {
+                Console.WriteLine($"Мінімальна ціна таксі - 50 грн, а ваша тільки {price} грн");
+            }
+            else
+            {
+                Console.WriteLine($"Ціна таксі: {price} грн");
+            }
+
+        }
+    }
+}
---
+++ TaxiCounter/TaxiCounter/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("TaxiCounter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TaxiCounter")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("d727a0d1-5127-4bfd-b7a8-5570556cb7d5")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
